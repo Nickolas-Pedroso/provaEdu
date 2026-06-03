482c4a23755759b37474560f898665a1ffa6c1e0
Nickolas Pedroso <nickolasaparecido@uol.com.br>
2026-06-02 22:23:02 -0300

    First commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,15 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
